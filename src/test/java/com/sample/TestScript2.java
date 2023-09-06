58bd6dc0441c1f899d7b79184affdf6fe39b2e51
ramya <ramya@RamyaG>
2023-09-06 10:29:18 +0530

    committing

---
+++ src/test/java/com/sample/TestScript2.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestScript2 {
+	
+		 @Test
+		 public void test1()
+		 {
+			 Reporter.log("test1");
+			 
+		 }
+		 @Test
+		 public void test2()
+		 {
+			 Reporter.log("test2");
+			 
+		 }
+}
